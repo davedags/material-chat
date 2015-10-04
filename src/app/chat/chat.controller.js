31102976250d3a82db9023a87825e69703f7aeeb
David D'Agostino <dave@dags.io>
2015-10-04 17:29:34 -0400

    initial angular code for sending chat and displaying

--- src/app/chat/chat.controller.js
+++ src/app/chat/chat.controller.js
@@ -3,6 +3,18 @@
 angular
     .module('ChatApp')
     .controller('ChatCtrl', function($scope) {
+	$scope.messages = [];
+	$scope.im = {};
 
+	$scope.sendChat = function(msg) {
+
+	    $scope.messages.push(msg);
+	    $scope.im = {};
+
+	    var chatBox = document.getElementById('chatBox');
+	    chatBox.scrollTop = 300 + 8 + ($scope.messages.length * 240);
+
+
+	}
     })
 
